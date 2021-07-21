6b832f96bf663aacbe636ded311896f672fe355f
Hakim Danial <79852485+rylaiftw@users.noreply.github.com>
2021-07-21 19:35:39 +0800

    fix footer in home.dart

--- lib/pages/cart/cart_screen.dart
+++ lib/pages/cart/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_ui/theme/colors.dart';
 import 'package:tiktok_ui/pages/cart/components/body.dart';
+import 'package:tiktok_ui/pages/cart/components/checkout_card.dart';
 
 class CartScreen extends StatelessWidget {
 
@@ -10,90 +11,7 @@ class CartScreen extends StatelessWidget {
       backgroundColor: black,
       appBar: buildAppBar(context),
       body: Body(),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 15,
-          horizontal: 30
-        ),
-        height: 120,
-        decoration: BoxDecoration(
-          color: grey,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(30),
-            topRight: Radius.circular(30),
-          ),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, -15),
-              blurRadius: 20,
-              color: Color(0xFFDADADA).withOpacity(0.15),
-            ),
-          ],
-        ),
-        child: SafeArea(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Row(
-              //   children: [
-              //     Container(
-              //       height: 40,
-              //       width: 40,
-              //       decoration: BoxDecoration(
-              //         color: grey,
-              //         borderRadius: BorderRadius.circular(10),
-              //       ),
-              //       child: Icon(Icons.receipt_long,
-              //       color: Colors.redAccent,),
-              //     ),
-              //     Spacer(),
-              //     Text('Add voucher code', style: TextStyle(
-              //       color: grey,
-              //     ),),
-              //     // here width is fixed thats why we u se const
-              //     const SizedBox(width: 10.0),
-              //     Icon(Icons.arrow_forward_ios, size: 12, color: grey,
-              //     ),
-              //   ],
-              // ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      text: 'Total:',
-                      style: TextStyle(fontSize: 16, color: black),
-                      children: [
-                      TextSpan(
-                      text: ' RM 224.76',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: black,
-                      fontWeight: FontWeight.bold),
-                      ),
-                      ],
-                    ),
-                  ),
-                    SizedBox(
-                      width: 150,
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ButtonStyle(),
-                      onPressed: (){},
-                        child: Text('Checkout',
-                        style: TextStyle(
-                          fontSize: 20
-                        ),),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+      bottomNavigationBar: CheckoutCard(),
     );
   }
 
@@ -128,4 +46,3 @@ class CartScreen extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/pages/cart/components/checkout_card.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_ui/theme/colors.dart';
+
+
+class CheckoutCard extends StatelessWidget {
+  const CheckoutCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          vertical: 15,
+          horizontal: 30
+      ),
+      height: 120,
+      decoration: BoxDecoration(
+        color: grey,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -15),
+            blurRadius: 20,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Row(
+            //   children: [
+            //     Container(
+            //       height: 40,
+            //       width: 40,
+            //       decoration: BoxDecoration(
+            //         color: grey,
+            //         borderRadius: BorderRadius.circular(10),
+            //       ),
+            //       child: Icon(Icons.receipt_long,
+            //       color: Colors.redAccent,),
+            //     ),
+            //     Spacer(),
+            //     Text('Add voucher code', style: TextStyle(
+            //       color: grey,
+            //     ),),
+            //     // here width is fixed thats why we u se const
+            //     const SizedBox(width: 10.0),
+            //     Icon(Icons.arrow_forward_ios, size: 12, color: grey,
+            //     ),
+            //   ],
+            // ),
+            //In Case We Are Going To Use Voucher Section
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    text: 'Total:',
+                    style: TextStyle(fontSize: 16, color: black),
+                    children: [
+                      TextSpan(
+                        text: ' RM 224.76',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(),
+                    onPressed: (){},
+                    child: Text('Checkout',
+                      style: TextStyle(
+                          fontSize: 20
+                      ),),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
