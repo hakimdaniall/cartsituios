4b01dd25345e92ef7e6030969c819d37c0b5451f
Hakim Danial <dev.hakimdanial@gmail.com>
2021-07-30 15:48:21 +0800

    merge commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tiktok_ui/appBehaviour/my_behaviour.dart';
 import 'package:tiktok_ui/pages/home.dart';
-<<<<<<< HEAD
-import 'package:tiktok_ui/pages/my_store/my_store_view_products/my_store_view_products.dart';
-=======
 import 'package:tiktok_ui/pages/notifications_messages/chat.dart';
 import 'package:tiktok_ui/pages/notifications_messages/messages.dart';
 import 'package:tiktok_ui/pages/notifications_messages/notification_list.dart';
 import 'package:tiktok_ui/pages/notifications_messages/notifications.dart';
->>>>>>> 7ac67183b51623aaab2e6bbcc6980a0007bf5461
 import 'package:tiktok_ui/pages/profile/edit_profile.dart';
 import 'package:tiktok_ui/pages/profile/myreview.dart';
 import 'package:tiktok_ui/pages/profile/profile_setting.dart';
@@ -22,8 +18,6 @@ import 'package:tiktok_ui/pages/wishlist/wishlist_screen.dart';
 import 'pages/details/details_screen.dart';
 import 'pages/review/review_screen.dart';
 import 'pages/splashScreen.dart';
-<<<<<<< HEAD
-=======
 import 'package:tiktok_ui/pages/cart/cart_screen.dart';
 import 'package:tiktok_ui/pages/checkout/checkout_screen.dart';
 import 'package:tiktok_ui/pages/creator_profile/creator_profile.dart';
@@ -35,7 +29,6 @@ import 'package:tiktok_ui/pages/my_store/my_store_view_products/my_store_view_pr
 import 'package:tiktok_ui/pages/my_store/my_store_view_products/view_product_inventory.dart';
 import 'package:tiktok_ui/pages/my_store/my_store_view_products/selected_product_inventory.dart';
 import 'pages/home.dart';
->>>>>>> 7ac67183b51623aaab2e6bbcc6980a0007bf5461
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -50,9 +43,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      home: ViewMyStoreProducts(),
-=======
       home: SplashScreen(),
       routes: {
         SplashScreen.id: (context) => SplashScreen(),
@@ -80,7 +70,6 @@ class MyApp extends StatelessWidget {
         TermOfUse.id: (context) => TermOfUse(),
         ProfileSetting.id: (context) => ProfileSetting(),
       },
->>>>>>> 7ac67183b51623aaab2e6bbcc6980a0007bf5461
       theme: ThemeData(
         textSelectionTheme: TextSelectionThemeData(
           cursorColor: Colors.white,
