b36c23ab14812e75726731b4d6b2d02fe9d89188
Hakim Danial <dev.hakimdanial@gmail.com>
2021-08-02 12:42:51 +0800

    details screen navigation added

--- lib/pages/cart/components/body.dart
+++ lib/pages/cart/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:tiktok_ui/pages/details/details_screen.dart';
 import 'cart_item_cart.dart';
 
 import 'package:tiktok_ui/models/Cart.dart';
@@ -12,34 +14,42 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: ListView.builder(
-        itemCount: demoCarts.length,
-        itemBuilder: (context, index) => Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Dismissible(
-            key: Key(demoCarts[index].product.id.toString()),
-            direction: DismissDirection.endToStart,
-            onDismissed: (direction) {
-              setState(() {
-                demoCarts.removeAt(index);
-              });
-            },
-            background: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Color(0xFFFFE6E6),
-                borderRadius: BorderRadius.circular(15),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            PageTransition(
+                type: PageTransitionType.topToBottom, child: DetailsScreen()));
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: ListView.builder(
+          itemCount: demoCarts.length,
+          itemBuilder: (context, index) => Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Dismissible(
+              key: Key(demoCarts[index].product.id.toString()),
+              direction: DismissDirection.endToStart,
+              onDismissed: (direction) {
+                setState(() {
+                  demoCarts.removeAt(index);
+                });
+              },
+              background: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFE6E6),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(
+                  children: [
+                    Spacer(),
+                    SvgPicture.asset("assets/icons/Trash.svg"),
+                  ],
+                ),
               ),
-              child: Row(
-                children: [
-                  Spacer(),
-                  SvgPicture.asset("assets/icons/Trash.svg"),
-                ],
-              ),
+              child: CartItemCard(cart: demoCarts[index]),
             ),
-            child: CartItemCard(cart: demoCarts[index]),
           ),
         ),
       ),
